e2cc1467f9b586afbaa33ec4b0642fa80fdf80ca
PiotrCiech <44189998+PiotrCiech@users.noreply.github.com>
2025-05-06 00:24:36 +0200

    Added Angular configuration (#33)
    * Added Angular configuration
    
    * Linter
    
    * Improved gitignore and tests
    
    * Fix
    
    * Pipeline update
    
    * Test coverage
    
    * Added package lock json
    
    * Fixing  CI
    
    * Fixing jasmine and pipeline
    
    * Fixed test coverage
    
    * Fixing test errors
    
    * Fixing new errors

---
+++ karma.conf.js
@@ -0,0 +1,51 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        jasmine: {
+        },
+        clearContext: false
+      },
+      jasmineHtmlReporter: {
+        suppressAll: true
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' },
+          { type: 'lcov' }
+        ],
+        check: {
+          global: {
+            statements: 0,
+            branches: 0,
+            functions: 0,
+            lines: 0
+          }
+        }
+      },
+      reporters: ['progress', 'kjhtml'],
+      customLaunchers: {
+        ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox', '--disable-gpu']
+        }
+      },
+      browsers: ['ChromeHeadless'],
+      restartOnFileChange: true,
+      failOnEmptyTestSuite: false,
+      browserNoActivityTimeout: 60000,
+      autoWatch: true,
+      singleRun: false
+    });
+  };
