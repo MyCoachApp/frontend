c82bec564a6f062f1ce239a10ea3fd738f2c6699
Piotr <piotrekciech@gmail.com>
2025-05-05 23:10:20 +0200

    Fixing  CI

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,13 +4,10 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('auth_token');
   
   if (token) {
-    // Jeśli token istnieje, dodaj go do nagłówków żądania
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
     return next(authReq);
-  }
-  
-  // Jeśli nie ma tokenu, kontynuuj bez modyfikacji
+  }  
   return next(req);
 };
