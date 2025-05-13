c02e8e2e0dc322d779e0907052be1bcc8905e4b8
Piotr <piotrekciech@gmail.com>
2025-05-13 09:34:46 +0200

    Fixed import

--- src/app/features/auth/components/social-login-form/social-login-form.component.spec.ts
+++ src/app/features/auth/components/social-login-form/social-login-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SocialLoginFormComponent } from './social-login-form.component';
 
-import { SocialLoginFormComponent } from './social-login.component';
 
 describe('SocialLoginComponent', () => {
   let component: SocialLoginFormComponent;
